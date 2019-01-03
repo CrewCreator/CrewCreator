b0daacacb6585b0367775cb809d3eaafa4e45c05
nathanjbrockway <nathanjbrockway@gmail.com>
2019-01-03 07:19:48 +0000

    fixed some stuff

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,20 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+  
+  def show
+    id = params[:id]
+    @course = Course.find(id)
+  end
+  
+  def new
+    @course = Course.new
+  end
+  
+  '''def create
+    @course = Course.create!(params.require(:course).permit(:name, :code, :description))
+    flash[:notice] = "#{@course.title} was successfully created."
+    redirect_to courses_path
+  end'''
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,3 @@
 class Project < ApplicationRecord
-  belongs_to :course
   validates_presence_of :name, :description, :students_rated, :total_interest
 end
