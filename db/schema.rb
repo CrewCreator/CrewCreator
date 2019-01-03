29c29d547eb673363f4c813dfdb8a773eda7a59c
Nicholas Tiner <ntiner1@tamu.edu>
2019-01-03 11:31:13 +0800

    Create project (#9)

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190103024038) do
+
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.integer "students_rated"
+    t.integer "total_interest"
+  end
 
 end
