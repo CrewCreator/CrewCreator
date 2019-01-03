aaea3e8c2f4d3f8c15a8c89860ad53dcc947022d
nathanjbrockway <nathanjbrockway@gmail.com>
2019-01-03 03:26:01 +0000

    course validation complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190103024324) do
+
+  create_table "courses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.string "code"
+  end
 
 end
