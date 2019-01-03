d67cf987940428e1c10fa9307d1509913e0cf87b
nathanjbrockway <nathanjbrockway@gmail.com>
2019-01-03 03:54:33 +0000

    removed extra migration lines

--- db/migrate/20190103013933_create_courses.rb
+++ db/migrate/20190103013933_create_courses.rb
@@ -1,9 +1,6 @@
 class CreateCourses < ActiveRecord::Migration[5.1]
   def change
     create_table :courses do |t|
-      t.string 'name'
-      t.string 'code'
-      t.text 'description'
       t.timestamps
     end
   end
