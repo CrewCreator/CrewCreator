ebc8b22f1187b9784f8c5450fe07a4dc2a3201c4
nathanjbrockway <nathanjbrockway@gmail.com>
2019-01-03 06:20:40 +0000

    Seed data set and course model outlined

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
+  belongs_to :course
   validates_presence_of :name, :description, :students_rated, :total_interest
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 courses = [
-{:name => 'Software Engineering', :code => 'CSCE 431', :decription => 'Making software great again'}
+{:name => 'Software Engineering', :code => 'CSCE 431', :description => 'Making software great again'},
+{:name => 'Progamming Studio', :code => 'CSCE 315', :description => 'Agile project based programming'},
+{:name => 'Software Security', :code => 'CSCE 489', :description => 'SaaS security and web routing'}
 ]
 
 courses.each do |course|
