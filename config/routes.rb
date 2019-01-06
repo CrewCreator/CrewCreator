0a3954e8dea4944c6971dd5676686575c792a370
Reed Hampton <reed.hampton@gmail.com>
2019-01-06 22:35:50 +0800

    Create admin (#22)
    * Initial model and rspec tests for Create Admin
    
    * Finished basic model validation with rspec testing
    
    * Finished rspec tests for Admin model
    
    * Includes code and tests for create and index view and controller methods for an Admin
    
    * Adds changes for logging in using sessioning and a simple gem for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources:courses
-  root :to => redirect('/courses')
+    root :to => redirect('/courses')
+  
+  resources :admins
+    get 'createaccount', to: 'admins#new', as: 'createaccount'
+    get 'account_profile', to: 'admins#show', as: 'accountprofile'
+    
+  resources :sessions, only: [:new, :create, :destroy]
+    get 'login', to: 'sessions#new', as: 'login'
+    get 'logout', to: 'sessions#destroy', as: 'logout'
 end
