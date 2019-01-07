4d85d946af3432cab0a4f5c95d2ac64d4f527e64
nathanjbrockway <nathanjbrockway@gmail.com>
2019-01-07 14:56:14 +0000

    updating new course view

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base	
+  #protect_from_forgery with: :exception	
+
+   helper_method :current_user	
+    def current_user	
+      if session[:user_id]	
+        if Admin.find(session[:user_id])	
+          @current_user ||= Admin.find(session[:user_id])	
+        end	
+      else	
+        @current_user = nil	
+      end	
+    end	
+
+   helper_method :is_admin	
+    def is_admin	
+      if current_user.nil? && !Admin.exists?(session[:user_id])	
+        redirect_to :controller => 'sessions', :action => 'new'	
+      end	
+    end	
+end
