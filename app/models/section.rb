b397a787e0d69929868cd8fc1d518d61f239d353
Alex Le <kawoofish@gmail.com>
2019-01-07 02:18:32 +0000

    Added length and numericality check to model file and fixed cucumber feature file

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,10 +1,8 @@
 class Section < ApplicationRecord
-<<<<<<< HEAD
-    has_many :projects, :moderators, :students
-    belongs_to :course, :admin
-    validates_presence_of :number
-=======
   #belongs_to :course
   validates_presence_of :number
->>>>>>> staging
+  
+  validates_length_of :number, maximum: 4
+  
+  validates_numericality_of :number
 end
