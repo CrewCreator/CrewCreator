c93896b769628f5a76248deb694df27da0d2896c
nathanjbrockway <nathanjbrockway@gmail.com>
2019-01-07 07:46:14 +0000

    merging with create_admin

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
   #has_many :sections
   #has_many :projects, through: :sections
   validates_presence_of :name, :code, :description
+  validates_uniqueness_of :code
 end
