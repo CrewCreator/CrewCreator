80e46ecc7dc0de0b35c7d991e16c224f84483170
Reed Hampton <reedhampton@tamu.edu>
2019-01-07 02:57:36 +0000

    adds route shortcuts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  
-  root :to => redirect('/home/index')
+
+  get 'home', to: 'home#index', as: 'home'
+  root :to => redirect('/home')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
