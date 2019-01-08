0a050fb564f8276449cc7a7dca7fa449c34463e4
nathanjbrockway <nathanjbrockway@gmail.com>
2019-01-08 05:26:37 +0000

    Create course finished

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -16,18 +16,48 @@ class CoursesController < ApplicationController
   end
   
   def create
-    @course = Course.create(params.require(:course).permit(:name, :code, :description)) #needs to check for uniqueness
+    @course = Course.create(params.require(:course).permit(:name, :code, :description))
     if @course.save
-      sections = ["200", "501", "502", "503", "505", "506"]
-      sections.each do |section|
-        if params[section.to_sym]
-          @course.sections.create(params.require(section.to_sym).permit(section.to_sym))
-        end
+      sections_added = ""
+      @section = @course.sections.build(sec_params)
+      
+      if @section.save
+        sections_added += "#{params[:section]} "
+      else
+        render new_course_path
+        return
       end
-      flash[:notice] = "#{@course.code} -- #{@course.name} was successfully created."
+
+      puts(params)
+      flash[:notice] = "#{@course.code} -- #{@course.name} was successfully created with sections#{sections_added}"
       redirect_to courses_path
-    elsif
-      render "/courses/new" #add warnings for required fields
+    else
+      if params[:course][:code] != "" and Course.where(code: params[:course][:code]).take
+        flash[:warning] = "Must have unique Course Code. #{params[:course][:code]} is already used."
+        render new_course_path
+      else
+        missing_attr = ""
+        i = 0
+        puts(params)
+        Course.new.attributes.keys.each do |var|
+          puts(var, i)
+          if params[:course][var.to_sym]=="" and i > 2
+            puts("add")
+            missing_attr += "#{var} "
+          end
+          i += 1
+        end
+        flash[:warning] = "#{missing_attr} required for creating a Course."
+        render new_course_path
+      end
     end
+  end
+  
+  private def req_params
+    params.require(:course).permit(:name, :code, :description)
+  end
+  
+  private def sec_params
+    params.require(:section).permit(:number)
   end
 end
