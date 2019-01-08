6ab176d2ce541e989dfe572b3ef0fc4a2c8055d2
Nick <ntiner1@tamu.edu>
2019-01-08 13:10:01 +0000

    DRYed projects sections

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,16 @@
 class ProjectsController < ApplicationController
   before_action :is_admin, only: [:new , :create]
+  before_action :find_section
   
   def index
-    @section = Section.find(params[:section_id])
     @projects = @section.projects
   end
   
   def new
-    @section = Section.find(params[:section_id])
     @project = Project.new
   end
   
   def create
-    @section = Section.find(params[:section_id])
     @project = @section.projects.build(project_params)
     @project.students_rated = 0
     @project.total_interest = 0
@@ -25,5 +23,9 @@ class ProjectsController < ApplicationController
   
   private def project_params
     params.require(:project).permit(:name, :description, :difficulty)
+  end
+  
+  private def find_section
+    @section = Section.find(params[:section_id])
   end
 end
