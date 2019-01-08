df0e00b87db335d74b38f6904f4af74952add787
Nick <ntiner1@tamu.edu>
2019-01-08 05:16:06 +0000

    Added projects to each section

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,19 +2,22 @@ class ProjectsController < ApplicationController
   before_action :is_admin, only: [:new , :create]
   
   def index
-    @projects = Project.all
+    @section = Section.find(params[:section_id])
+    @projects = @section.projects
   end
   
   def new
+    @section = Section.find(params[:section_id])
     @project = Project.new
   end
   
   def create
-    @project = Project.new(project_params)
+    @section = Section.find(params[:section_id])
+    @project = @section.projects.build(project_params)
     @project.students_rated = 0
     @project.total_interest = 0
     if @project.save
-      redirect_to projects_path
+      redirect_to course_section_projects_path
     else
       render 'new'
     end
