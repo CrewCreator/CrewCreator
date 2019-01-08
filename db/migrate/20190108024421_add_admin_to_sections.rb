ebf5ee963bc51b1c872d3034fc0385cb237135f6
Nick <ntiner1@tamu.edu>
2019-01-08 05:17:51 +0000

    Removed unnessecary migrations

--- db/migrate/20190108024421_add_admin_to_sections.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminToSections < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :sections, :admin
-  end
-end
--- db/migrate/20190108044424_remove_admin_from_sections.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAdminFromSections < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference :sections, :admin, foreign_key: true
-  end
-end
