3a0389920a8701910e23ab3c677ca03cb2e3b8cd
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2019-01-09 11:15:56 +0800

    Rspec tests done

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,16 @@
 class Team < ApplicationRecord
 
-  validates :name, presence: {message: "Name cannot be empty"}, uniqueness: {message: "Team name already taken"}
+  # VALID_URL_REGEX = /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
+  VALID_URL_REGEX = /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
+
+  validates_presence_of :name, :description
+  validates_length_of :description, minimum: 30
+  validates_length_of :description, maximum: 500
+
+  validates_format_of :staging_link, :with => VALID_URL_REGEX, :on => :create
+  validates_format_of :production_link, :with => VALID_URL_REGEX, :on => :create
+  validates_format_of :version_control_link, :with => VALID_URL_REGEX, :on => :create
+  validates_format_of :project_management_link, :with => VALID_URL_REGEX, :on => :create
+
 
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{Team.new(name: "Crew Creator", description: "The best team ever wahahahaahahahahahahhahaa", project_management_link: "www.pivotaltracker.com",
+                   version_control_link: "www.github.com",
+                   staging_link: "www.herokuapp.com/staging",
+                   production_link:"www.herokuapp.com/production")}
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid if description is too long" do
+    subject.description = "a" * 501
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid if description is too short" do
+    subject.description = "short message"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid if links is improper format" do
+    subject.staging_link = "aaaaaaaaaaaaaaaa"
+    expect(subject).to_not be_valid
+  end
+# TODO: Add test to guard against duplicate team names
 end
