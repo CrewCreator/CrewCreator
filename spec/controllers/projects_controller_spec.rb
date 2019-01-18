39682c7cb5d5e08cbf0541b14e8707624b5b2da2
Nick <ntiner1@tamu.edu>
2019-01-18 18:44:34 +0000

    Updated project controller rspec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -18,42 +18,60 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
-  describe "POST #create" do
-    it "expect http redirect" do
-      atr = attributes_for(:project)
-      post :create, params: { section_id: @section.id, project: atr }
-      expect(response).to have_http_status(:redirect)
-    end
-  end
-
-  describe "POST #destroy" do
-    it "expect http redirect" do
-      project = create(:project)
-      post :destroy, params: { id: project.id }
-      expect(response).to have_http_status(:redirect)
-    end
-  end
-
   describe "GET #new" do
-    it "returns http success" do
+    before(:each) do
       get :new, params: { section_id: @section.id }
-      expect(response).to have_http_status(:redirect)
+    end
+    
+    it "renders new template" do
+      expect(response).to render_template("new")
+    end
+    
+    it "redirect to new project page" do
+      expect(response).to redirect_to(new_section_project_path)
     end
   end
   
   describe "GET #edit" do
+    before(:each) do
+      @project = create(:project)
+      get :edit, params: { id: @project.id }
+    end
+    
     it "renders edit template" do
-      project = create(:project)
-      get :edit, params: { id: project.id }
       expect(response).to render_template("edit")
+    end
+    
+    it "redirect to edit project page" do
+      expect(response).to redirect_to(edit_project_path)
+    end
+    
+    it "assigns @project" do
+      expect(assigns(:project)).to eq(@project)
+    end
+  end
+
+  describe "POST #create" do
+    it "redirect to projects page" do
+      atr = attributes_for(:project)
+      post :create, params: { section_id: @section.id, project: atr }
+      expect(response).to redirect_to(section_projects_path)
+    end
+  end
+
+  describe "POST #destroy" do
+    it "redirect to projects page" do
+      project = create(:project)
+      post :destroy, params: { id: project.id }
+      expect(response).to redirect_to(section_projects_path)
     end
   end
   
   describe "POST #update" do
-    it "expect http redirect" do
+    it "redirect to projects page" do
       project = create(:project)
       post :update, params: {:id => project.id }
-      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(section_projects_path)
     end
   end
 
