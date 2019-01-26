ee545996f95ee547bb2a7faf007c446c844c1402
Reed Hampton <reed.hampton@gmail.com>
2019-01-26 14:08:46 -0600

    Adds student model with email validation across both admin and student accounts (#62)

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
-class Admin < ApplicationRecord
-  
+class Student < ApplicationRecord
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     PASSWORD_FORMAT = /\A
                       (?=.*\d)           # Must contain a digit
