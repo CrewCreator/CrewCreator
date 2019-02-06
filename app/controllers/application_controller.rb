790edecb88df7269293e720113aff6fa32864f6b
nathanjbrockway <nathanjbrockway@gmail.com>
2019-02-06 17:58:21 +0000

    updated user type checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
     def current_user
       if session[:user_id]
-        if session[:is_admin] == true
+        if session[:user] == "admin"
+          puts session[:user]
           if Admin.exists? (session[:user_id])
             @current_user ||= Admin.find(session[:user_id])
           else
             @current_user = nil
           end
-        elsif session[:is_admin] == false
+        elsif session[:user] == "student"
           if Student.exists? (session[:user_id])
             @current_user ||= Student.find(session[:user_id])
           else
@@ -26,14 +27,30 @@ class ApplicationController < ActionController::Base
     
   helper_method :is_admin
     def is_admin
-      if session[:is_admin] == false || current_user.nil?
-        redirect_to :controller => 'sessions', :action => 'new'
+      if session[:user] != "admin" || current_user.nil?
+        redirect_to controller: 'sessions', action: 'new'
       end
     end
     
   helper_method :is_admin_html
     def is_admin_html
-      if session[:is_admin] == false || current_user.nil?
+      if session[:user] != "admin" || current_user.nil?
+        false
+      else
+        true
+      end
+    end
+    
+  helper_method :is_instructor
+    def is_instructor
+      if session[:user] != "instructor" || current_user.nil?
+        redirect_to controller: 'sessions', action: 'new'
+      end
+    end
+    
+  helper_method :is_instructor_html
+    def is_instructor_html
+      if session[:user] != "instructor" || current_user.nil?
         false
       else
         true
@@ -52,20 +69,20 @@ class ApplicationController < ActionController::Base
   helper_method :is_user
     def is_user
       if current_user.nil?
-        redirect_to :controller => 'sessions', :action => 'new'
+        redirect_to controller: 'sessions', action: 'new'
       end
     end
     
   helper_method :is_student
     def is_student
-      if session[:is_admin] == true || current_user.nil?
-        redirect_to :controller => 'sessions', :action => 'new'
+      if session[:user] != "student" || current_user.nil?
+        redirect_to controller: 'sessions', action: 'new'
       end
     end
     
   helper_method :is_student_html
     def is_student_html
-      if session[:is_admin] == true || current_user.nil?
+      if session[:user] != "student" || current_user.nil?
         false
       else
         true
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,30 +6,39 @@ class SessionsController < ApplicationController
     if user = Admin.find_by_email(params[:email])
       if user.authenticate(params[:password])
         session[:user_id] = user.id
-        session[:is_admin] = true
+        session[:user] = :admin
         redirect_to controller: 'home', action: 'index'
       else
-        flash[:notice] = "Failed to Log In! Password is invalid for #{params[:email]}"
+        flash[:notice] = "Failed to Log In! Password is invalid"
         redirect_to '/login'
       end
     elsif user = Student.find_by_email(params[:email])
       if user.authenticate(params[:password])
         session[:user_id] = user.id
-        session[:is_admin] = false
+        session[:user] = :student
         redirect_to controller: 'home', action: 'index'
       else
-        flash[:notice] = "Failed to Log In! Password is invalid for #{params[:email]}"
+        flash[:notice] = "Failed to Log In! Password is invalid"
+        redirect_to '/login'
+      end
+    elsif user = Instructor.find_by_email(params[:email])
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        session[:user] = :instructor
+        redirect_to controller: 'home', action: 'index'
+      else
+        flash[:notice] = "Failed to Log In! Password is invalid"
         redirect_to '/login'
       end
     else
-      flash[:notice] = "Failed to Log In! Account with email #{params[:email]} not found"
+      flash[:notice] = "Failed to Log In! Account not found"
       redirect_to '/login'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    session[:is_admin] = nil
+    session[:user] = nil
     redirect_to '/home'
   end
 end
