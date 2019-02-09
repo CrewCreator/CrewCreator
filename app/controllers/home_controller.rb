5fcf9803bc785b07e0f9d5f51b2e534821456ac3
Nick <ntiner1@tamu.edu>
2019-02-09 17:38:19 +0000

    Add dashboard test, consolidated dashboard view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    if is_admin_html
+      @sections = Section
+    end
+    
+    if is_student_html
+      @sections = current_user.sections
+    end
   end
 end
